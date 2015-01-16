a670dcebeba389d4910eb47594e55cadfcdda87e
gmanPark <gman.park@nhn.com>
2015-01-16 20:17:26 +0900

    source upload

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 #!/usr/bin/env node
 'use strict';
 
-var runnable = require('./lib/nts-readme.js');
-console.log(process.cwd());
+var oThread = function(){this.initialize.apply(this,arguments)};
 
-runnable.exec(__dirname);
+oThread.prototype = {
+    initialize: function(){
+        this.module = require('./lib/module.js');
+        this.runnable = require('./lib/nts-readme.js');
+        var read =  this.module.rl;
+        read.setPrompt('nts-readme > ');
+        read.prompt();
+
+        read.on('line', function(line) {
+            switch(line.trim()) {
+                case 'hello':
+                    this.runnable.exec(this.module, __dirname);
+                    break;
+                default:
+                    console.log('Say what? I might have heard `' + line.trim() + '`');
+                    break;
+            }
+            read.prompt();
+        }).on('close', function() {
+            console.log('Have a great day!');
+            process.exit(0);
+        });
+
+//        if(this.isFileOverwrite(this.module.rl)){
+//            console.log('finish');node
+//            this.runnable.exec(this.module, __dirname);
+//        }
+    },
+    isFileOverwrite: function(rl){
+        rl.question("Do you want Overwrite? (y/n)", function(answer){
+            if(answer.toLowerCase().indexOf('y') >= 0){
+                return true;
+            }
+        })
+        return false;
+    }
+}
+
+new oThread();
