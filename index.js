3b594004dfaba87f6f9b1a51d8ee31c830d46c7d
박지만 <gman.park@nhn.com>
2015-01-21 22:57:25 +0900

    1차 완성

--- index.js
+++ index.js
@@ -7,38 +7,24 @@ oThread.prototype = {
     initialize: function(){
         this.module = require('./lib/module.js');
         this.runnable = require('./lib/nts-readme.js');
-        var read =  this.module.rl;
-        read.setPrompt('nts-readme > ');
-        read.prompt();
-
-        read.on('line', function(line) {
-            switch(line.trim()) {
-                case 'hello':
-                    this.runnable.exec(this.module, __dirname);
-                    break;
-                default:
-                    console.log('Say what? I might have heard `' + line.trim() + '`');
-                    break;
-            }
-            read.prompt();
-        }).on('close', function() {
-            console.log('Have a great day!');
-            process.exit(0);
-        });
-
-//        if(this.isFileOverwrite(this.module.rl)){
-//            console.log('finish');node
-//            this.runnable.exec(this.module, __dirname);
-//        }
+    },
+    run: function(){
+        if(this.module.fs.existsSync('readme.md')){
+            this.isFileOverwrite(this.module.rl);
+        }else{
+            this.runnable.exec(this.module, __dirname);
+        }
     },
     isFileOverwrite: function(rl){
-        rl.question("Do you want Overwrite? (y/n)", function(answer){
-            if(answer.toLowerCase().indexOf('y') >= 0){
-                return true;
+        console.log("Do you want to overwrite Readme.md ? ( y / n ) : ");
+        rl.on('line',function(answer){
+            if(answer.match(/^y(es)?$/i)){
+                this.runnable.exec(this.module, __dirname);
+            }else{
+                process.exit(0);
             }
-        })
-        return false;
+        }.bind(this));
     }
 }
 
-new oThread();
+new oThread().run();
